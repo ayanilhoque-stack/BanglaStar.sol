4acf414fb0dd1d0065015567082733856631533d
ayanilhoque-stack <ayanilhoque@gmail.com>
2026-02-09 17:35:48 +0600

    Update BanglaStar.sol

--- BanglaStar.sol
+++ BanglaStar.sol
@@ -1,1 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract BanglaStarToken {
+
+    // Basic token info
+    string public name = "BanglaStar";
+    string public symbol = "BST";
+    uint8 public decimals = 18;
+
+    uint256 public totalSupply;
+    address public owner;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor() {
+        owner = msg.sender;
+
+        // 🔢 Total Supply = 90 Billion
+        totalSupply = 90_000_000_000 * 10 ** decimals;
+
+        balanceOf[owner] = totalSupply;
+        emit Transfer(address(0), owner, totalSupply);
+    }
+
+    function transfer(address to, uint256 amount) public returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public returns (bool) {
+        require(balanceOf[from] >= amount, "Balance too low");
+        require(allowance[from][msg.sender] >= amount, "Allowance too low");
+
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        allowance[from][msg.sender] -= amount;
+
+        emit Transfer(from, to, amount);
+        return true;
+    }
+}
